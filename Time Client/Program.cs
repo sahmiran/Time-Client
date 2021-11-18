c90b6889c65fefa530664e9245c2cf8cf6eb0bb6
hasan <hasan@DESKTOP-C0EJST9>
2021-11-18 15:13:37 +0300

    Add project files.

---
+++ Time Client/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Time_Client
+{
+    class Program
+    {
+        private static Socket _clientSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            LoopConnect();
+            SendLoop();
+            Console.ReadLine();
+        }
+
+        private static void SendLoop()
+
+            //backgroundworker eklemeye çalış nasıl olacak bak incele.
+        {
+                Console.Write("Enter a request: ");
+                string req = Console.ReadLine();
+                byte[] buffer = Encoding.ASCII.GetBytes(req);
+                _clientSocket.Send(buffer);
+
+                byte[] receiveBuf = new byte[1024];
+                int rec = _clientSocket.Receive(receiveBuf);
+                byte[] data = new byte[rec];
+                Array.Copy(receiveBuf, data, rec);
+                Console.WriteLine(Encoding.ASCII.GetString(data));
+        }
+
+        private static void LoopConnect()
+        {
+            int attempts = 0;
+            while (!_clientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    _clientSocket.Connect(IPAddress.Loopback, 100);
+                }
+                catch (SocketException)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Connection attemps: "+ attempts.ToString());
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Connected");
+        }
+    }
+}
